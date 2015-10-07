f94f9c072001fd323d572ae9b77d32b9ea607491
tejswinikathar <tejswini.kathar@mindstix.com>
2015-10-07 18:49:45 +0530

    modified item detail page

--- GroceryStock/app/controllers/grocery.js
+++ GroceryStock/app/controllers/grocery.js
@@ -19,3 +19,59 @@ function minusFun(){
 		alert("Please enter valid Quantity");
 	}
 }
+
+function selectUnitFun(){
+	log.trace("selectUnitFun function");
+	// //alert("selectUnitFun");
+	// //core.Navigator.openView("unitList");
+	 // var unitWin=Alloy.createController('unitList').getView('unitview');
+  // unitWin.top="100%";
+   // unitWin.height="100%";
+  // // win4.width="100%";
+  // unitWin.animate({
+  	// top: "0%",
+  	// duration:300
+  // });
+  // $.mainView.add(unitWin);
+  $.unitview.setVisible(true);
+}
+
+function unitClickFun(e){
+	log.trace("unitClickFun" + JSON.stringify(e));
+	var item1 = $.units.SelectedIndex;
+	log.trace(item1);
+	//alert(e.itemIndex);
+	switch(e.itemIndex) {
+		
+	case 0:
+		
+		$.unitTxt.value="Kilogram";
+		backToDeatil();
+		break;
+	case 1:
+
+		$.unitTxt.value="Gram";
+		backToDeatil();
+		break;
+	case 2:
+		
+		$.unitTxt.value="Liter";
+		backToDeatil();
+		break;
+	case 3:
+		
+		$.unitTxt.value="Dozen";
+		backToDeatil();
+		break;
+	case 4:
+		
+		$.unitTxt.value="Piece";
+		backToDeatil();
+		break;
+	}
+}
+
+function backToDeatil(){
+	log.trace("backToDeatil");
+	 $.unitview.setVisible(false);
+}
